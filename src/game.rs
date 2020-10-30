9eb9de7a5d41c4bb0bdad4ac805228d9766dd5b3
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-10-30 01:14:41 -0700

    possible strats for better state transitions

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,5 @@
 use crate::board::Board;
-use crate::errors::InvalidError;
+use crate::errors::{InvalidError, InvalidMoveError};
 use crate::players::Player;
 use std::convert::TryInto;
 
@@ -89,14 +89,80 @@ pub struct InitialGame<P: Player> {
     game: GameCore<P>,
 }
 
+
 pub struct InProgressGame<P: Player> {
     game: GameCore<P>,
 }
-impl<P: Player> InProgressGame<P> {}
+
+pub struct FinishedGame<P: Player> {
+    game: GameCore<P>,
+}
+
+/// # POSSIBLE Strats for modelling multi-class state transitions.
+/// ## Strat 1: Wrap state in enum
+/// Use enum to wrap game states. This requires `match` littered  everywhere
+/// and potential invalid transitions.
+enum StatefulGame<P: Player>{
+    Initial (InitialGame<P>),
+    InProgress (InProgressGame<P>),
+    Finished (FinishedGame<P>),
+}
+type BeginResult<P: Player> = Result<StatefulGame<P>, InvalidError>;
+/// ## Strat 2: Type & Trait spam
+/// For a given combination of possible output state, define a trait that binds them.
+/// Constrait state output to this new trait. This ensures only allowed valid states
+/// and can enforce expectations at compile time.
+/// However, if expectations cannot be specified at compile time, you've got more work than it solves.
+/// Cleanest and best if have compile time expectations.
+pub trait WillBePlayable {}
+impl<P: Player> WillBePlayable for InitialGame<P>{}
+impl<P: Player> WillBePlayable for InProgressGame<P>{}
+type BeginResultV2<G: WillBePlayable> = Result<G, InvalidError>;
+/// ## Strat 3: Group All potential states and wrap in result.
+/// Either wrap possible states in a tuple of `Option<State>` or an enum on state. Wrap output in Result.
+/// Can enforce expectation at runtime using match or indexing in tuple. For runtime expectations,
+/// match and if-else on index will do the job. This improves on trait hack in-terms of being able 
+/// to easily identify and operate on expected type at runtime. Because reversing from trait to 
+/// concrete is not easy if possible.
+// 3.1
+type InitialOrInProgressGame<P: Player> = (Option<InitialGame<P>>, Option<InProgressGame<P>>);
+type BeginResultV3p1<P: Player> = Result<InitialOrInProgressGame<P>, InvalidError>;
+// 3.2
+enum IniOrProgEnum<P:Player>{
+    Initial(InitialGame<P>),
+    InProgress (InProgressGame<P>),
+}
+type BeginResultV3p2<P: Player> = Result<IniOrProgEnum<P>, InvalidError>;
+// ---
+
 impl<P: Player> InitialGame<P> {
     pub fn new(words: &Vec<String>) -> Result<Self, InvalidError> {
         let game = GameCore::new(words)?;
         return Ok(InitialGame { game });
+    }
+
+    pub fn add_player_team_one(&mut self, player: P) {
+        self.game.add_player_team_one(player)
+    }
+
+    pub fn add_player_team_two(&mut self, player: P) {
+        self.game.add_player_team_two(player)
+    }
+
+    pub fn set_team_one_spymaster(&mut self, ind: usize) -> Result<(), InvalidError> {
+        self.game.set_team_one_spymaster(ind)
+    }
+
+    pub fn set_team_two_spymaster(&mut self, ind: usize) -> Result<(), InvalidError> {
+        self.game.set_team_two_spymaster(ind)
+    }
+
+    pub fn get_team_one_score(&self) -> u8 {
+        self.game.get_team_one_score()
+    }
+
+    pub fn get_team_two_score(&self) -> u8 {
+        self.game.get_team_two_score()
     }
 
     pub fn begin(self) -> (Option<Self>, Result<InProgressGame<P>, InvalidError>) {
@@ -109,6 +175,28 @@ impl<P: Player> InitialGame<P> {
                 "Not enough players. Each team needs 2 players atleast and a chosen spymaster.",
             )),
         );
+    }
+}
+
+impl<P: Player> InProgressGame<P> {
+    pub fn add_player_team_one(&mut self, player: P) {
+        self.game.add_player_team_one(player)
+    }
+
+    pub fn add_player_team_two(&mut self, player: P) {
+        self.game.add_player_team_two(player)
+    }
+
+    pub fn get_team_one_score(&self) -> u8 {
+        self.game.get_team_one_score()
+    }
+
+    pub fn get_team_two_score(&self) -> u8 {
+        self.game.get_team_two_score()
+    }
+
+    pub fn unravel(&mut self, ind: usize) -> Result<(Option<Self>, Result<FinishedGame<P>, InvalidError>), InvalidMoveError>{
+        todo!()
     }
 }
 
@@ -138,8 +226,8 @@ mod tests {
         assert_eq!(game.game.get_team_one_score(), 8);
         assert_eq!(game.game.get_team_two_score(), 8);
 
-        game.game.add_player_team_one(SimplePlayer::new("p1"));
-        game.game.add_player_team_two(SimplePlayer::new("p2"));
+        game.add_player_team_one(SimplePlayer::new("p1"));
+        game.add_player_team_two(SimplePlayer::new("p2"));
 
         let res = game.begin();
         // make sure it didn't transition to playing due to lack of players
@@ -150,8 +238,8 @@ mod tests {
         // get our game obj back.
         game = res.0.unwrap();
 
-        game.game.add_player_team_one(SimplePlayer::new("p3"));
-        game.game.add_player_team_two(SimplePlayer::new("p4"));
+        game.add_player_team_one(SimplePlayer::new("p3"));
+        game.add_player_team_two(SimplePlayer::new("p4"));
         // make sure it didn't transition to playing due to lack of spymasters
         let res = game.begin();
         // get our game obj back.
@@ -161,9 +249,9 @@ mod tests {
             "game shouldn't be ready without spymaster and 1 more player."
         );
         // it should work now.
-        let res = game.game.set_team_one_spymaster(0);
+        let res = game.set_team_one_spymaster(0);
         assert!(res.is_ok());
-        let res = game.game.set_team_two_spymaster(1);
+        let res = game.set_team_two_spymaster(1);
         assert!(res.is_ok());
         let res = game.begin();
         assert!(res.1.is_ok());
